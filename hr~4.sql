26372ebcb0b4ca1cccf761fc4ee655031325ddb6
raiyanharthy <138356970+raiyanharthy@users.noreply.github.com>
2023-07-06 09:15:58 +0400

    Update hr~4.sql

--- hr~4.sql
+++ hr~4.sql
@@ -1,44 +1,44 @@
---using the avg and sum functions
-SELECT AVG(salary), MAX(salary), MIN(salary), SUM(salary) FROM employees WHERE job_id LIKE '%REP%';
+--using the to_char function with dates
+SELECT last_name, TO_CHAR(hire_date, 'fmDD Month YYYY') AS HIREDATE FROM employees;
 
---using the min and max functions
-SELECT MIN(hire_date), MAX(hire_date) FROM employees;
+--using the to_char function with numbers
+SELECT TO_CHAR(salary, '$99,999.00') SALARY FROM employees WHERE last_name = 'Ernst';
 
---using the count function
-SELECT COUNT(*) FROM employees WHERE department_id = 50;
+--using the to_char and to_date function with rr date format
+SELECT last_name, TO_CHAR(hire_date, 'DD-Mon-YYYY') FROM employees WHERE hire_date < TO_DATE('01-Jan-90','DD-Mon-RR');
 
-SELECT COUNT(commission_pct) FROM employees WHERE department_id = 80;
+--nesting functions
+SELECT last_name,
+UPPER(CONCAT(SUBSTR (LAST_NAME, 1, 8), '_US')) FROM employees WHERE department_id = 60;
 
---using the distinct keyword
-SELECT COUNT(DISTINCT department_id) FROM employees;
+--using the nvl function
+SELECT last_name, salary, NVL(commission_pct, 0),
+(salary*12) + (salary*12*NVL(commission_pct, 0)) AN_SAL FROM employees;
 
---group functions and null values
-SELECT AVG(commission_pct) FROM employees;
+--using the nvl2 function
+SELECT last_name, salary, commission_pct,
+NVL2(commission_pct,
+'SAL+COMM', 'SAL') income FROM employees WHERE department_id IN (50, 80);
 
-SELECT AVG(NVL(commission_pct, 0)) FROM employees;
+--using the nullif function
+SELECT first_name, LENGTH(first_name) "expr1",
+last_name, LENGTH(last_name) "expr2",
+NULLIF(LENGTH(first_name), LENGTH(last_name)) result FROM employees;
 
---using the group by clause
-SELECT department_id, AVG(salary) FROM employees GROUP BY department_id ;
+--using the coalesce function
+SELECT last_name, employee_id,
+COALESCE(TO_CHAR(commission_pct),TO_CHAR(manager_id),
+'No commission and no manager') FROM employees;
 
-SELECT AVG(salary) FROM employees GROUP BY department_id ;
+--using the case expression
+SELECT last_name, job_id, salary,
+CASE job_id WHEN 'IT_PROG' THEN 1.10*salary
+WHEN 'ST_CLERK' THEN 1.15*salary
+WHEN 'SA_REP' THEN 1.20*salary 
+ELSE salary END "REVISED_SALARY" FROM employees;
 
---using the group by clause on multiple columns
-SELECT department_id, job_id, SUM(salary) FROM employees WHERE department_id > 40 GROUP BY department_id, job_id
-ORDER BY department_id;
-
---illegal queries using group functions
-SELECT department_id, COUNT(last_name) FROM employees;
-
-SELECT department_id, job_id, COUNT(last_name) FROM employees GROUP BY department_id;
-
---using the having clause
-SELECT department_id, MAX(salary) FROM employees
-GROUP BY department_id HAVING MAX(salary)>10000 ;
-
-SELECT job_id, SUM(salary) PAYROLL FROM employees
-WHERE job_id NOT LIKE '%REP%' GROUP BY job_id HAVING SUM(salary) > 13000
-ORDER BY SUM(salary);
-
---display the maximum average salary by nesting group functions
-SELECT MAX(AVG(salary)) FROM employees GROUP BY department_id;
-
+--using the decode function (like case but when is not needed)
+SELECT last_name, job_id, salary,
+DECODE(job_id, 'IT_PROG', 1.10*salary,
+'ST_CLERK', 1.15*salary,
+'SA_REP', 1.20*salary, salary) REVISED_SALARY FROM employees;
